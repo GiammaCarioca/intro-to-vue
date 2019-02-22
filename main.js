2d6f000beac0aeae777e472e3bf2d9468193c177
Thiago Giammattey <giammattey@gmail.com>
2019-02-22 17:00:01 -0300

    event handling: v-on

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-//Add an array of sizes to the data and use v-for to display them in a list.
-
 var app = new Vue({
 	el: '#app',
 	data: {
@@ -17,6 +15,6 @@ var app = new Vue({
 				variantColor: 'blue'
 			}
 		],
-		sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
+		cart: 0
 	}
 });
